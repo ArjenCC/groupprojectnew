507c8be89ef1465de1276a18c50acbbba5e5f39b
ArjenCC <113308394+ArjenCC@users.noreply.github.com>
2023-03-23 21:12:57 -0400

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 module.exports = {
     //local MongoDB deployment ->
     //"URI": "mongodb://localhost/mydb2"
-  "URI":"mongodb+srv://Saheed22:Saheed22@cluster0.lirry.mongodb.net/Survey?retryWrites=true&w=majority",
+  "URI":"mongodb+srv://Arjen:6D9lukYFyINH868e@mongodbserver.vuncwmt.mongodb.net/Survey?retryWrites=true&w=majority",
   "Secret": 'SomeSecret'
   };
   
